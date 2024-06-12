0c57a1a183c2a6e66c2d92710a661a1f71312566
thabo4587 <142932054+thabo4587@users.noreply.github.com>
2024-06-12 19:32:50 +0200

    added error page

---
+++ src/Errorpage.js
@@ -0,0 +1,14 @@
+// src/components/ErrorPage.js this 
+
+import React from 'react';
+
+function ErrorPage() {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>Data fetching failed...</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
